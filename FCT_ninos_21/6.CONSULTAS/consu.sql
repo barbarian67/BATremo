83fe581a457e1dec0d8ce0226d0d62c4ccdb0a7f
Jaime Canillas <jaime.canillas@iesfernandoaguilar.es>
2022-12-06 19:26:50 +0100

    Cambiamos de FCT a BAT y añadimos pizarrazos

--- FCT_ninos_21/6.CONSULTAS/consu.sql
+++
@@ -1,6 +0,0 @@
---HERRAMIENTAS MÁS DEMANDADAS.
-select nomHerra as "Herramientas Demandadas" from TOOLBOX inner join HERRAMIENTAS on idHerra=HERRAMIENTA having count(herramienta) = (select max(count(herramienta)) from TOOLBOX group by HERRAMIENTA) group by nomHerra;
-
-
---
-select nomjer, nomPer from PERSONAS inner join JERARQUIAS on idJer=JERARQUIA order by JERARQUIA;
